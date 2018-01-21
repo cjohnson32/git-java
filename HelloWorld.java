c3c8a00596c6b18645c44a2df71074a6c117be50
Cody Johnson <cjohnson32@stu.jsu.edu>
2018-01-21 16:26:08 -0600

    Added Class3

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,14 @@ public class HelloWorld {
 		Class1 c1 = new Class1();        
 		Class2 c2 = new Class2();        
 		System.out.println("Hello, World!");    
-	}    
+	}
 	private void f1() {        
 		// Placeholder method (v1.0)    
 	}
 	private void f2() {        
 		// Placeholder method (v2.0)    
-	}	
+	}
+	private void f3() {        
+		// Placeholder method (v3.0)    
+	}
 }
